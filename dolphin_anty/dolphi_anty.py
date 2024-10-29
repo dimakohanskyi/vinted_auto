61d909fb1cc9f06c299979a2a99a6932b38ebd29
dimakohanskyi <dimakohanskyi@gmail.com>
2024-10-29 11:45:39 +0100

    impliment select_product_marka_process, select_product_size_process, select_product_condition_process
    functions, continue work on add all data to оголошення

--- dolphin_anty/dolphi_anty.py
+++ dolphin_anty/dolphi_anty.py
@@ -78,13 +78,8 @@ def get_products_images(profile_id):
 
 
 def upload_product_images(driver, fake_image_paths):
-    """
-      Завантажує зображення для конкретного продукту.
-      Повертає True, якщо всі зображення успішно завантажені, інакше False.
-    """
 
     successful_uploads = 0
-
     for fake_image_el in fake_image_paths:
         if fake_image_el:
             try:
@@ -107,6 +102,8 @@ def upload_product_images(driver, fake_image_paths):
                         ))
                     )
                     new_add_img_btn.click()
+                    random_timesleep()
+                    random_scroll(driver=driver)
 
                 # Додавання зображення через pyautogui
                 pyautogui.press('enter')
@@ -123,13 +120,13 @@ def upload_product_images(driver, fake_image_paths):
                 if image_name:
                     # Вибір імені файлу зображення
                     pyautogui.hotkey('command', 'shift', 'g')
-                    time.sleep(2)
+                    random_timesleep()
                     pyautogui.write(image_name)
-                    time.sleep(2)
+                    random_timesleep()
                     pyautogui.press('enter')
                     time.sleep(2)
                     pyautogui.press('enter')
-                    time.sleep(4)
+                    random_timesleep()
 
                 # Збільшуємо лічильник успішних завантажень
                 successful_uploads += 1
@@ -149,6 +146,7 @@ def upload_product_images(driver, fake_image_paths):
 
 def select_category_process(driver, product_cat_mas):
     try:
+        selected_categories = set()
         # Wait for the dropdown to be present and click it
         category_vinted_dropdown = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "c-input__content"))
@@ -157,42 +155,125 @@ def select_category_process(driver, product_cat_mas):
 
         # Loop through categories
 
-        while True:
+        while len(selected_categories) < len(product_cat_mas):
             # Wait for dropdown items to be present
             dropdown_items = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.web_ui__List__list li"))
             )
 
-            found_category = False
-
             for item in dropdown_items:
                 try:
                     category_name = item.find_element(By.CLASS_NAME, "web_ui__Cell__title").text
 
-                    if category_name in product_cat_mas:
-                        print(f"Attempting to select category: {category_name}")
+                    if category_name in product_cat_mas and category_name not in selected_categories:
                         item.click()
                         random_timesleep()  # Random sleep after selection
                         print(f"Категорія '{category_name}' була обрана.")
-                        found_category = True
-                        break  # Break the loop if a category was selected
+
+                        selected_categories.add(category_name)
+                        break
 
                 except Exception as ex:
                     print("Stale element reference encountered. Refetching items...")
                     break  # Break to re-fetch dropdown items
-                except Exception as ex:
-                    print(f"Error encountered while processing category '{category_name}': {ex}")
-                    break  # Exit the for loop to refetch items
-
-            if found_category:
-                # If a category was found and selected, we need to refetch the items again
-                continue  # Loop back to fetch the items again to ensure we have the latest
 
     except Exception as ex:
         print("Timeout while waiting for elements.")
 
     finally:
         print("Exiting category selection process.")
+
+
+def select_product_marka_process(driver, marka_cat_mas):
+    try:
+        marka_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='brand-select-dropdown-input']"))
+        )
+        marka_input.click()
+
+        pyautogui.write(marka_cat_mas.title())
+        random_timesleep()
+
+        marka_dropdown_items = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".web_ui__Cell__cell.web_ui__Cell__clickable"))
+        )
+
+        brands = [element.text for element in marka_dropdown_items]
+
+        for brand in marka_dropdown_items:
+            if brand.text.strip() == marka_cat_mas.title():
+                random_timesleep()
+                brand.click()
+                random_timesleep()
+                print(f'Selected brand: {brand.text}')
+                return
+
+        print(f'Brand {marka_cat_mas.title()} not found in the list.')
+
+    except Exception as ex:
+        print(f"Error with select marka: {ex}")
+
+
+def select_product_size_process(driver, product_size_mas):
+    try:
+        size_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#size_id"))
+        )
+
+        size_input.click()
+        random_timesleep()
+
+        size_dropdown_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, ".web_ui__Cell__cell.web_ui__Cell__default.web_ui__Cell__clickable"))
+        )
+        for size_element in size_dropdown_elements:
+            print(f"Available size: {size_element.text}")  # Вивести всі доступні розміри
+            if size_element.text == product_size_mas:  # Порівняти з бажаним розміром
+                size_element.click()  # Клікнути на потрібний розмір
+                random_timesleep()
+                print(f"Selected size: {size_element.text}")
+                break
+        else:
+            print(f"Size '{product_size_mas}' not found in the dropdown.")
+
+    except Exception as ex:
+        print(f"Error with select size: {ex}")
+
+
+def select_product_condition_process(driver, product_condition_mas):
+    # TODO need updates and test
+    print(f"product_condition_mas --  {product_condition_mas}")
+    try:
+        condition_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input#status_id"))
+        )
+        condition_input.click()
+        random_timesleep()
+
+        condition_dropdown_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".web_ui__Cell__cell.web_ui__Cell__default.web_ui__Cell__clickable"))
+        )
+
+        for condition_element in condition_dropdown_elements:
+            condition_text = condition_element.text.strip().lower()
+            print(f"condition_text {condition_text}")
+
+            if condition_text == product_condition_mas.strip().lower():
+                condition_element.click()
+                random_timesleep()
+                print(f"Selected condition: {condition_element.text}")
+                break
+        else:
+            print(f"Condition '{product_condition_mas}' not found in the dropdown.")
+
+    except Exception as ex:
+        print(f"Error with select condition: {ex}")
+
+
+
+
+
 
 
 def dolphin_aut(profile_id):
@@ -222,18 +303,46 @@ def dolphin_aut(profile_id):
     try:
         user_products = get_products_images(profile_id=profile_id)
 
+        # Набір для відстеження завантажених продуктів
+        uploaded_product_ids = set()
+
         for user_product in user_products:
-
-            images = user_product.get('images', [])
-            fake_image_paths = [image['fake_image_path'] for image in images]
-            # upload_product_images(driver, fake_image_paths)
-
-            if not fake_image_paths:
-                print("No images to upload for this product.")
+            if user_product['id'] in uploaded_product_ids:
+                print(f"Продукт '{user_product['title']}' вже був завантажений, пропускаємо.")
                 continue
 
-            # product_cat_mas = user_product['category']
-            # select_category_process(driver, product_cat_mas)
+            random_timesleep()
+
+            product_cat_mas = user_product['category']
+            select_category_process(driver, product_cat_mas)
+            #
+            # marka_cat_mas = user_product['company']
+            # select_product_marka_process(driver, marka_cat_mas)
+
+            # product_size_mas = user_product['size']
+            # select_product_size_process(driver, product_size_mas)
+
+            product_condition_mas = user_product['condition']
+            select_product_condition_process(driver, product_condition_mas)
+
+
+
+
+            # images = user_product.get('images', [])
+            # fake_image_paths = [image['fake_image_path'] for image in images]
+
+            # if fake_image_paths:
+            #     print("Uploading images...")
+            #     upload_success = upload_product_images(driver, fake_image_paths)
+            #     if upload_success:
+            #         print(f"All images for product '{user_product['title']}' uploaded successfully.")
+            #         uploaded_product_ids.add(user_product['id'])
+            #     else:
+            #         print(f"Some images for product '{user_product['title']}' failed to upload.")
+            # else:
+            #     print("No images to upload for this product.")
+            #
+            # print(f"Finished processing product '{user_product['title']}'.")
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -253,7 +362,6 @@ def random_scroll(driver, min_pause=1, max_pause=5, min_scroll=100, max_scroll=5
 def random_timesleep(min_seconds=1, max_seconds=5):
     sleep_time = random.uniform(min_seconds, max_seconds)
     time.sleep(sleep_time)
-    print(f"Slept for {sleep_time:.2f} seconds.")
 
 
 
